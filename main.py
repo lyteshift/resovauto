234e5064a7af162ac82f2c2d6cacfd12ee8147b0
lyteshift <georgecb05@gmail.com>
2024-12-09 04:07:30 +0000

    title formatting

--- main.py
+++ main.py
@@ -9,9 +9,7 @@ title = """[bold orange1]
        ___  __   ___  __      __   ___  __   __                 ___  __  
         |  / _` |__  / _`    |__) |__  /__` /  \ \  /  /\  |  |  |  /  \ 
         |  \__> |___ \__>    |  \ |___ .__/ \__/  \/  /  \ \__/  |  \__/ 
-[/bold orange1][bold]
-                        Written by George Cash-Blackmore
-                           Open to feature requests[/bold][red bold] <3[/red bold]
+[/bold orange1][bold]       Written by George Cash-Blackmore     [bold blue]v1.0.0-alpha.1 "King Arthur"[/bold blue]
 """
 
 # // RESOVAUTO
