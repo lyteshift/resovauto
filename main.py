202fd1d02bbcab5fcdd4047933d76a8ef88d94fe
lyteshift <georgecb05@gmail.com>
2024-12-10 05:35:23 +0000

    added cool visuals added history added help page

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from rich import print
 from rich.progress import track
 import os
 
-title = """                                                          [bold blue]/[/bold blue]   
-[bold white]           ___  __   ___  __  [/bold white][bold blue][bold blue]    __   ___  __   __      /[/bold blue][bold red]          ___  __  [/bold red]
-[bold white]            |  / _` |__  / _` [/bold white][bold blue][bold blue]   |__) |__  /__` /  \ \  / [/bold blue][bold red] /\  |  |  |  /  \ [/bold red]
-[bold white]            |  \__> |___ \__> [/bold white][bold blue][bold blue]   |  \ |___ .__/ \__/  \/  [/bold blue][bold red]/  \ \__/  |  \__/ [/bold red]
-[bold white]           Written by George Cash-Blackmore[/bold white]    [bold red]v1.0.0-alpha.3 "Chlorophytum"[/bold red]
-                                                        [bold red]/[/bold red]"""
+title = """                                                          [bold deep_sky_blue1]/[/bold deep_sky_blue1]   
+[bold white]           ___  __   ___  __  [/bold white][bold deep_sky_blue1][bold deep_sky_blue1]    __   ___  __   __      /[/bold deep_sky_blue1][bold deep_pink2]          ___  __  [/bold deep_pink2]
+[bold white]            |  / _` |__  / _` [/bold white][bold deep_sky_blue1][bold deep_sky_blue1]   |__) |__  /__` /  \ \  / [/bold deep_sky_blue1][bold deep_pink2] /\  |  |  |  /  \ [/bold deep_pink2]
+[bold white]            |  \__> |___ \__> [/bold white][bold deep_sky_blue1][bold deep_sky_blue1]   |  \ |___ .__/ \__/  \/  [/bold deep_sky_blue1][bold deep_pink2]/  \ \__/  |  \__/ [/bold deep_pink2]
+[bold white]           Written by George Cash-Blackmore[/bold white]    [bold deep_pink2]v1.0.0-alpha.3 "Chlorophytum"[/bold deep_pink2]
+                                                        [bold deep_pink2]/[/bold deep_pink2]"""
 
 # // RESOVAUTO
 # Written by George Cash-Blackmore
@@ -45,6 +45,8 @@ tomorrow = str(datetime.datetime.today()+datetime.timedelta(days=1)).split()[0]
 
 #Cost Values
 item_extras_value = 4.00
+
+just_launched = True
 
 api_calls = 0
 
@@ -91,7 +93,7 @@ def get_rooms_verbose():
 
 def count_daily_customers(date):
     count = 0
-    for rooms in track(get_rooms(),"    [bold green]Getting daily customers...[/bold green]"):
+    for rooms in track(get_rooms(),"    [bold sea_green2]Getting daily customers...[/bold sea_green2]"):
 
         avail_req = "https://api.resova.co.uk/v1/availability/calendar?start_date="+date+"&end_date="+date+"&item_ids="+str(rooms)
 
@@ -104,7 +106,7 @@ def count_daily_customers(date):
 
 def get_daily_instances(date):
     all_instances = []
-    for rooms in track(get_rooms(),"    [bold green]Getting booking data...[/bold green] "):
+    for rooms in track(get_rooms(),"    [bold sea_green2]Getting booking data...[/bold sea_green2] "):
 
         avail_req = "https://api.resova.co.uk/v1/availability/calendar?start_date="+date+"&end_date="+date+"&item_ids="+str(rooms)
         for items in resova(avail_req)["data"][date]["items"]:
@@ -119,7 +121,7 @@ def get_presales(date):
     total_presales = 0
     item_extras_total = 0.00
 
-    for instance in track(daily_instances,"    [bold green]Calculating pre-sales...[/bold green]"):
+    for instance in track(daily_instances,"    [bold sea_green2]Calculating pre-sales...[/bold sea_green2]"):
         
         booking = resova("https://api.resova.co.uk/v1/availability/instance/"+instance)
 
@@ -150,7 +152,7 @@ def get_same_day(date):
     daily_instances = get_daily_instances(date)
     count = 0
 
-    for instance in track(daily_instances,"    [bold green]Calculating pre-sales...[/bold green]"):
+    for instance in track(daily_instances,"    [bold sea_green2]Counting bookings...    [/bold sea_green2]"):
         booking = resova("https://api.resova.co.uk/v1/availability/instance/"+instance)
         if booking["bookings"] != []:
             if booking["bookable"] == False:
@@ -160,59 +162,84 @@ def get_same_day(date):
                             count += 1
     return count
 
+last_output = ""
+last_output_msg = ""
+last_output_date = ""
+
+def set_last_output(output,msg,date):
+    global last_output
+    global last_output_msg
+    global last_output_date
+    global just_launched
+    
+    last_output = output
+    last_output_msg = msg
+    last_output_date = date   
+    just_launched = False
+
+
+
+def print_footer():
+    print("""   [deep_sky_blue1][/deep_sky_blue1][bold white on deep_sky_blue1] {last_output} [/bold white on deep_sky_blue1][deep_pink2 on deep_sky_blue1][/deep_pink2 on deep_sky_blue1][bold white on deep_pink2] {msg} {date} [/bold white on deep_pink2][deep_pink2][/deep_pink2]
+""".format(last_output=last_output,msg=last_output_msg,date=last_output_date))
 
 def main():
     while True:
         cls()
         print(title)
-        print("[bold blue]   Welcome to the Resovauto CLI[/bold blue]")
-        print("""   [orange1]Choose an option from below to begin...[/orange1]
+        print("""   [orange1][/orange1][bold black on orange1]Choose an option from below to begin...[/bold black on orange1][orange1][/orange1]
+              
    [1] Daily customers (exc. RA/ID)  [2] Daily presales  [3] Same-day bookings 
-   [red][0] Exit (ctrl+c)[/red]
+   [deep_pink2][0] Exit (ctrl+c)[/deep_pink2]  [bold sea_green2]\[help][/bold sea_green2]
             """)
+        
+        global just_launched
+        if not just_launched:
+            print_footer()
+        
         userinput = input(": ")
         if userinput == "0":
             break
         if userinput == "1":
             cls()
             print(title)
-            print("[bold white]   For which day?[/bold white]")
-            print("""   [orange1]Choose an option from below...[/orange1]
-   \[today] \[yesterday] \[YYYY-MM-DD]
+            print("""   [orange1][/orange1][bold black on orange1]For which day?[/bold black on orange1][orange1][/orange1]
+   \[[bold deep_sky_blue1]t[/bold deep_sky_blue1]oday] \[[bold deep_sky_blue1]y[/bold deep_sky_blue1]esterday] \[YYYY-MM-DD]
                   """)
             date = input(": ")
             print("")
-            if date == "today":
+            if date == "today" or date == "t":
                 date = today
-            if date == "yesterday":
+            if date == "yesterday" or date ==  "y":
                 date = yesterday
             try:
                 
                 cls()
                 print(title)
                 print("")
-                print(count_daily_customers(date),"customers visited on",date)
+                total_customers = count_daily_customers(date)
+                print(total_customers,"customers visited on",date)
+                set_last_output(total_customers,"customers visited on",date)
                 if input("Continue? [y/n]? ") == "n":
                     break
             except:
-                #cls()
+                cls()
                 print(title)
-                print("\n[red]ERROR! Maybe you typed the date wrong?[/red]")
+                print("\n[deep_pink2]ERROR! Maybe you typed the date wrong?[/deep_pink2]")
                 if input("Try again? [y/n]? ") == "n":
                     break
         if userinput == "2":
             
             cls()
             print(title)
-            print("[bold white]   For which day?[/bold white]")
-            print("""   [orange1]Choose an option from below...[/orange1]
-   \[today] \[yesterday] \[YYYY-MM-DD]
+            print("""   [orange1][/orange1][bold black on orange1]For which day?[/bold black on orange1][orange1][/orange1]
+   \[[bold deep_sky_blue1]t[/bold deep_sky_blue1]oday] \[[bold deep_sky_blue1]y[/bold deep_sky_blue1]esterday] \[YYYY-MM-DD]
                   """)
             date = input(": ")
             print("")
-            if date == "today":
+            if date == "today" or date == "t":
                 date = today
-            if date == "yesterday":
+            if date == "yesterday" or date ==  "y":
                 date = yesterday
             try:
                 
@@ -221,39 +248,40 @@ def main():
                 presales = get_presales(date)
                 print("\n",presales," total presale drinks on",date)
                 print("£",presales*4.00,"total value @ £4.00/token")
+                set_last_output(str("£"+str(presales*4.00)),"total presales value on",date)
                 if input("Continue? [y/n]? ") == "n":
                     break
             except:
                 
                 cls()
                 print(title)
-                print("\n[red]ERROR! Maybe you typed the date wrong?[/red]")
+                print("\n[deep_pink2]ERROR! Maybe you typed the date wrong?[/deep_pink2]")
                 if input("Try again? [y/n]? ") == "n":
                     break
         if userinput == "3":
             cls()
             print(title)
-            print("[bold white]   For which day?[/bold white]")
-            print("""   [orange1]Choose an option from below...[/orange1]
-   \[today] \[yesterday] \[YYYY-MM-DD]
+            print("""   [orange1][/orange1][bold black on orange1]For which day?[/bold black on orange1][orange1][/orange1]
+   \[[bold deep_sky_blue1]t[/bold deep_sky_blue1]oday] \[[bold deep_sky_blue1]y[/bold deep_sky_blue1]esterday] \[YYYY-MM-DD]
                   """)
             date = input(": ")
             print("")
-            if date == "today":
+            if date == "today" or date == "t":
                 date = today
-            if date == "yesterday":
+            if date == "yesterday" or date ==  "y":
                 date = yesterday
             try:
                 cls()
                 print(title)
                 same_day = get_same_day(date)
                 print(same_day,"same day bookings on",date)
+                set_last_output(same_day,"same day bookings on",date)
                 if input("Continue? [y/n]? ") == "n":
                     break
             except:
                 cls()
                 print(title)
-                print("\n[red]ERROR! Maybe you typed the date wrong?[/red]")
+                print("\n[deep_pink2]ERROR! Maybe you typed the date wrong?[/deep_pink2]")
                 if input("Try again? [y/n]? ") == "n":
                     break
         if userinput == "debug":
@@ -276,6 +304,20 @@ def main():
 
             if input(": "):
                 pass
-
+        if userinput == "help":
+            cls()
+            print(title)
+            print("""   [sea_green2][/sea_green2][bold black on sea_green2]Help Page                                                                          [/bold black on sea_green2][sea_green2][/sea_green2]
+   In the main menu, type one of the listed numbers to access a function. Must functions
+   will ask you for a date, you may type "today", "yesterday", or any date in the format
+   "YYYY-MM-DD", where YYYY is the year, MM, is the month, and DD is the day. 
+   
+   If you find your input isn't working, the most likely problem is a misformed input,
+   so make sure you've included a dash "-" on your date input, and don't have any spaces
+   " " before or after the input.
+   
+   """)
+            if input("Return to menu? [y/n]? ") == "n":
+                break
 
 main()
